ba1b1ac3f3a260c883309b86debeb5c6f3beb042
ds10x2 <padasol@khu.ac.kr>
2023-08-27 01:56:32 +0900

    first commit

---
+++ src/main/java/com/ds10x2/MyBlog/Controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.ds10x2.MyBlog.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world!";
+    }
+}
---
+++ src/test/java/com/ds10x2/MyBlog/MyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds10x2.MyBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
