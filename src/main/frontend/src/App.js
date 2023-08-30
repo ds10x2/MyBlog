4eca54e93721bde0eeeb6f714d7c100ef8b9e80a
ds10x2 <padasol@khu.ac.kr>
2023-08-30 15:24:36 +0900

    frontend : map으로 변경

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 
 function Nav(){
+  const data = ["Home", "Information", "Portfolio", "Contents", "Link"];
+
   return (
     <div class="navigation">
-      <a href="#"><div class="text">Home</div></a>
-      <a href="#"><div class="text">Information</div></a>
-      <a href="#"><div class="text">Portfolio</div></a>
-      <a href="#"><div class="text">Contents</div></a>
-      <a href="#"><div class="text">Link</div></a>
+      {data.map((item) => <a href="#"><div class="text">{item}</div></a>)}
     </div>
   );
 }
