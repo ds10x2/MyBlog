276731d6e433a39d38d6fd279784549a2043b384
ds10x2 <padasol@khu.ac.kr>
2023-08-31 22:04:03 +0900

    frontend : css 및 메인 타이틀 수정

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,19 +1,41 @@
+import { useRef, useState, useEffect } from 'react';
 import './App.css';
+import { InView, inView } from 'react-intersection-observer';
 
 function Nav(){
   const data = ["Home", "Information", "Portfolio", "Contents", "Link"];
-
   return (
-    <div class="navigation">
-      {data.map((item) => <a href="#"><div class="text">{item}</div></a>)}
-    </div>
+      <div className="navigation">
+        {data.map((item) => <a href="#"><div className="text">{item}</div></a>)}
+      </div>
   );
 }
 
+function Title(){
+
+  const title = "MY RESPONSIBILITY";
+  const [titleIsView, setTitleIsView] = useState(false);
+
+
+  return(
+  <InView as="div" className="main" onChange={(inView, entry)=> {
+    setTitleIsView(inView);
+  }}>
+    <h2 className={`main-title${titleIsView ? "view" : ""}`}>title</h2>
+  </InView>
+  );
+}
+
+
 function App() {
   return (
-    <div>
-      <Nav></Nav>
+    <div className="App">
+      <div className='Header'>
+        <Nav></Nav>
+      </div>
+      <div className='contents'>
+        <Title></Title>
+      </div>
     </div>
   )
 }
